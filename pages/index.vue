d1cc8eb9ae70c0c62ab55d4e988e617884bc8573
Nguyen Quang Trung <quangtrung.uet@gmail.com>
2019-10-20 15:54:59 +0700

    rm footer pulsa

--- pages/index.vue
+++ pages/index.vue
@@ -14,15 +14,8 @@
       <div class="container has-text-grey">
         <div class="content has-text-centered">
           <p>
-            <strong class="has-text-grey">Pulsar</strong> is a small group of <a href="#team" v-smooth-scroll>developers</a>.
+            <strong class="has-text-grey">XTeam</strong> is a small group of <a href="#team" v-smooth-scroll>developers</a>.
             Our projects are free to use and made with care.
-            <br>
-            If you love what we do, please consider <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=38BCHG4HYDWH8" title="Donate">donating</a>.
-          </p>
-          <p>
-            <a class="icon" href="https://github.com/pulsardev" title="GitHub">
-              <i class="fa fa-github"></i>
-            </a>
           </p>
         </div>
       </div>
